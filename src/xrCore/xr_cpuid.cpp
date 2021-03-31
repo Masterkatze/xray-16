ffeed772c524ec7c2bfd0d7b6c728e55bbe625e2
Masterkatze <masterkatze@gmail.com>
2021-03-31 22:25:08 +0300

    Rename cpuid.h header, fix __cpuid call

--- src/xrCore/xr_cpuid.cpp
+++ src/xrCore/xr_cpuid.cpp
@@ -1,7 +1,11 @@
 #include "stdafx.h"
 #pragma hdrstop
 
-#include "cpuid.h"
+#if (defined(XR_PLATFORM_LINUX) || defined(XR_PLATFORM_FREEBSD)) && defined(XR_COMPILER_GCC)
+#include <cpuid.h>
+#endif
+
+#include "xr_cpuid.h"
 
 #include <array>
 #include <bitset>
@@ -25,8 +29,8 @@ void nativeCpuId(int regs[4], int i)
 {
 #ifdef XR_PLATFORM_WINDOWS
     __cpuid((int *)regs, (int)i);
-#elif (defined(XR_PLATFORM_LINUX) || defined(XR_PLATFORM_FREEBSD)) && defined(GCC)
-    __cpuid((int)i, (int *)regs);
+#elif (defined(XR_PLATFORM_LINUX) || defined(XR_PLATFORM_FREEBSD)) && defined(XR_COMPILER_GCC)
+    __cpuid((int)i, regs[0], regs[1], regs[2], regs[3]);
 #elif defined(XR_PLATFORM_LINUX) || defined(XR_PLATFORM_FREEBSD)
 #if defined(XR_ARCHITECTURE_X86) || defined(XR_ARCHITECTURE_X64)
     asm volatile("cpuid" :
